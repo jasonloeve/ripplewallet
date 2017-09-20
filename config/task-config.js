4f3ca5152317accb544a9199fa68c6813663b85e
jasonloeve <jasonloeve@gmail.com>
2017-09-20 18:23:36 +1200

    Updating build
    sfgfdhdfh

---
+++ config/task-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  html        : true,
+  images      : true,
+  fonts       : true,
+  static      : true,
+  svgSprite   : true,
+  ghPages     : true,
+  stylesheets : true,
+
+  javascripts: {
+    entry: {
+      // files paths are relative to
+      // javascripts.dest in path-config.json
+      app: ["./app.js"],
+    },
+    publicPath: 'javascripts',
+    provide: {
+      $: 'jquery',
+      jQuery: 'jquery'
+    }
+  },
+
+  browserSync: {
+    server: {
+      // should match `dest` in
+      // path-config.json
+      baseDir: 'public'
+    }
+  },
+
+  production: {
+    rev: true
+  }
+}
