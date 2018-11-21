027c88e29e3bddd584e50ef05b361c65a7440b5a
jasonloeve <jasonloeve@gmail.com>
2018-11-21 21:00:12 +1300

    Add and configure particles.js to hero section
    Add and configure particles.js to hero section

--- src/components/SectionHero.vue
+++ src/components/SectionHero.vue
@@ -49,6 +49,9 @@
 </template>
 
 <script>
+
+    // import particles from 'particlesjs'
+
     export default {
         name: 'SectionStats',
         data () {
@@ -59,9 +62,137 @@
                 bannerAd: 'http://via.placeholder.com/350x500',
                 bannerAdLink: 'https://placeholder.com'
             }
+        },
+        mounted () {
+            require('particles.js');
+            this.$nextTick(() => {
+                this.initParticlesJS()
+            })
+        },
+        methods: {
+            initParticlesJS () {
+                /* eslint-disable */
+                particlesJS('particles-js', {
+                    "particles": {
+                        "number": {
+                            "value": 80,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
+                        },
+                        "color": {
+                            "value": "#ffffff"
+                        },
+                        "shape": {
+                            "type": "circle",
+                            "stroke": {
+                                "width": 1,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 4
+                            },
+                            "image": {
+                                "src": "",
+                                "width": 0,
+                                "height": 0
+                            }
+                        },
+                        "opacity": {
+                            "value": 0,
+                            "random": false,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 0,
+                            "random": false,
+                            "anim": {
+                                "enable": false,
+                                "speed": 40,
+                                "size_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": true,
+                            "distance": 192.40944730386272,
+                            "color": "#ffffff",
+                            "opacity": 0.28,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 2,
+                            "direction": "none",
+                            "random": false,
+                            "straight": false,
+                            "out_mode": "out",
+                            "bounce": false,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
+                        }
+                    },
+                    "interactivity": {
+                        "detect_on": "canvas",
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "grab"
+                            },
+                            "onclick": {
+                                "enable": false,
+                                "mode": "push"
+                            },
+                            "resize": true
+                        },
+                        "modes": {
+                            "grab": {
+                                "distance": 133.99274002972194,
+                                "line_linked": {
+                                    "opacity": 1
+                                }
+                            },
+                            "bubble": {
+                                "distance": 400,
+                                "size": 40,
+                                "duration": 2,
+                                "opacity": 8,
+                                "speed": 3
+                            },
+                            "repulse": {
+                                "distance": 200,
+                                "duration": 0.4
+                            },
+                            "push": {
+                                "particles_nb": 4
+                            },
+                            "remove": {
+                                "particles_nb": 2
+                            }
+                        }
+                    },
+                    "retina_detect": true
+                })
+            }
         }
     }
 </script>
+
+
+
+
+
+
+
 
 <style>
 
